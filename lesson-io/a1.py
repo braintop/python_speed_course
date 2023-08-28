eeb59e4cac9ac7d4cf8adef3b525d24694817237
braintop <braintop7@gmail.com>
2023-08-28 19:43:06 +0300

    add a1.py

---
+++ lesson-io/a1.py
@@ -0,0 +1,15 @@
+# This Python program demonstrates basic input, calculation, and output operations.
+
+# Print a simple greeting to the console
+print("Hello world")
+# Read an integer input from the user and store it in the variable num1
+num1 = int(input("Enter the first number: "))
+
+# Read another integer input from the user and store it in the variable num2
+num2 = int(input("Enter the second number: "))
+
+# Calculate the sum of the two input numbers
+sum1 = num1 + num2 
+
+# Print the result along with an explanatory message
+print("You have now recorded two numbers whose sum is:", sum1)
